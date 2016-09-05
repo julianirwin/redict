0afa833ce56fc29f9ce3064459a7440b26c8edcc
Julian Irwin <julian.irwin@gmail.com>
2016-09-05 08:28:52 -0500

    made package files have redict contents

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
-setup(name='reglean',
-      version='0.0.1',
-      description=u"Glean metadata from a filename",
-      classifiers=['Topic :: Text Processing'],
+setup(name='redict',
+      version='0.0.0',
+      description=u"Dictionary-like object that uses regular expressions instead of keys",
+      classifiers=[],
       keywords='re',
       author=u"Julian Irwin",
       author_email='julian.irwin@gmail.com',
-      url='https://github.com/julianirwin/reglean',
+      url='https://github.com/julianirwin/redict',
       license='MIT',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
